6608f423ea9f916c744029abe00dfe175445f14b
RawalAnuma <rawalanuma@gmail.com>
2026-07-23 17:00:10 +0545

    Common components for text widget

---
+++ lib/core/customs/custom_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+  const CustomText({
+    super.key,
+    required this.text,
+    this.fontSize = 20,
+    this.color = Colors.black,
+    this.fontWeight = FontWeight.w400,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        color: color,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
+}
